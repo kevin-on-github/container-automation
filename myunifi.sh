8bf392037a3722af4f81e1d1d75032da3bc5a220
Kevin <86642014+kevin-on-github@users.noreply.github.com>
2023-01-06 16:22:08 -0500

    fix typo

--- myunifi.sh
+++ myunifi.sh
@@ -6,9 +6,9 @@
 docker run -d \
   --name=unifi-controller \
   -e TZ='America/New_York' \
-  -p 192.168.1.211:8443:8443 \
-  -p 192.168.1.211:3478:3478/udp \
-  -p 192.168.1.211:8080:8080 \
+  -p 8443:8443 \
+  -p 3478:3478/udp \
+  -p 8080:8080 \
   -v "unifi:/unifi" \
   --restart unless-stopped \
   jacobalberty/unifi:5.6.39
