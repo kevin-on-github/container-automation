4d084bbfbb344522609f2500def43f6811af6508
Kevin <86642014+kevin-on-github@users.noreply.github.com>
2023-03-31 12:40:08 -0400

    fix for podman

--- myportainer.sh
+++ myportainer.sh
@@ -8,10 +8,9 @@ podman volume create portainer_data
 
 # Run the container.
 podman run -d \
-  -p 8000:8000 -p 9000:9000 \
-  --name=portainer --restart=always --pull=always \
-  -v /run/podman/podman.sock:/run/podman/podman.sock \
-  --mount type=volume,source=portainer_data,destination=/data \
-  portainer/portainer-ce
+    -p 9443:9443 --privileged \
+    -v /run/podman/podman.sock:/var/run/docker.sock:Z \
+    --mount type=volume,source=portainer_data,destination=/data \
+    portainer/portainer-ce
 
 # Note the path to podman.sock. Will have to manually specify this in the portainer endpoint configuration.
